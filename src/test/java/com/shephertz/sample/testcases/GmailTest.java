8a18b9d6c42017ff98fa2d97842b6ea9f6f272bc
Pankaj Singh <pankaj.singh@shephertz.co.in>
2020-02-17 17:58:11 +0530

    dfdsf Signed-off-by: Pankaj Singh <pankaj.singh@shephertz.co.in>

--- src/test/java/com/shephertz/sample/testcases/GmailTest.java
+++ src/test/java/com/shephertz/sample/testcases/GmailTest.java
@@ -8,12 +8,16 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 import org.testng.annotations.BeforeClass;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Wait;
@@ -29,26 +33,67 @@ public class GmailTest {
 
 	@BeforeClass
 	public void setUp() {
-		WebDriverManager.chromedriver().setup();
+		/*
+		 * WebDriverManager.chromedriver().setup();
+		 * 
+		 * System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+		 * 
+		 * ChromeOptions options = new ChromeOptions();
+		 * options.addArguments("--headless");
+		 * options.addArguments("--start-maximized");
+		 * options.addArguments("--window-size=1200,800"); driver = new
+		 * ChromeDriver(options);
+		 * System.out.println("---Intialize Chrome Driver------------");
+		 * driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		 * driver.get("http://accounts.google.com/");
+		 * 
+		 * try { Thread.sleep(1000); } catch (InterruptedException e) { // TODO
+		 * Auto-generated catch block e.printStackTrace(); } SignInPage =
+		 * PageFactory.initElements(driver, GmailSignInPage.class);
+		 */
+		
+		
+		
+		
+		
+		 new DesiredCapabilities();
+	       URL serverurl;
+	try {
+	serverurl = new URL("http://localhost:9515");
+	       DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+	       driver = new RemoteWebDriver(serverurl,capabilities);
+	WebDriverManager.chromedriver().setup();
+	} catch (MalformedURLException e) {
+	// TODO Auto-generated catch block
+	e.printStackTrace();
+	}
 
-		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
-	
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("headless");
+	// System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+	//
+	// ChromeOptions options = new ChromeOptions();
+	// options.addArguments("headless");
+	//
+	// driver = new ChromeDriver(options);
+	// driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
+	driver.get("http://accounts.google.com/");
+	SignInPage = PageFactory.initElements(driver, GmailSignInPage.class);
 
-		driver = new ChromeDriver(options);
-		driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
-		driver.get("http://accounts.google.com/");
-		SignInPage = PageFactory.initElements(driver, GmailSignInPage.class);
+		
 	}
 
 	@Test
 	public void testLoginGmail() throws InterruptedException {
+		System.out.println("======testLoginGmail=====Started=");
 		SignInPage.txt_Email.sendKeys("test.shephertz@gmail.com");
+		System.out.println("Email Enterd");
 		SignInPage.btn_next.click();
-		Thread.sleep(2000);
+		System.out.println("Click Next and wait for some time");
+		Thread.sleep(1000);
 		SignInPage.txt_Psw_Email.sendKeys("test.shephertz");
+		System.out.println("Password Enterd");
 		SignInPage.btn_next2.click();
+
+		System.out.println("=====Click Sign in=");
 	}
 
 	@AfterClass
