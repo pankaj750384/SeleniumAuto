a69c94ed130021ef4f63d6efc0d91448550c1559
Naveen Goswami <naveen.goswami@shephertz.com>
2020-02-17 18:50:11 +0530

    change
    Signed-off-by: Naveen Goswami <naveen.goswami@shephertz.com>

--- src/test/java/com/shephertz/sample/testcases/GmailTest.java
+++ src/test/java/com/shephertz/sample/testcases/GmailTest.java
@@ -105,9 +105,18 @@ public class GmailTest {
         System.setProperty("webdriver.chrome.driver", "/home/devops/chromedriver/chromedriver");
         // Add options to Google Chrome. The window-size is important for responsive sites
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("headless");
-        options.addArguments("window-size=1200x600");
-        WebDriver driver = new ChromeDriver(options);
+//        options.addArguments("headless");
+//        options.addArguments("window-size=1200x600");
+        options.addArguments("start-maximized"); // https://stackoverflow.com/a/26283818/1689770
+        options.addArguments("enable-automation"); // https://stackoverflow.com/a/43840128/1689770
+        options.addArguments("--headless"); // only if you are ACTUALLY running headless
+        options.addArguments("--no-sandbox"); //https://stackoverflow.com/a/50725918/1689770
+        options.addArguments("--disable-infobars"); //https://stackoverflow.com/a/43840128/1689770
+        options.addArguments("--disable-dev-shm-usage"); //https://stackoverflow.com/a/50725918/1689770
+        options.addArguments("--disable-browser-side-navigation"); //https://stackoverflow.com/a/49123152/1689770
+        options.addArguments("--disable-gpu"); //https://stackoverflow.com/questions/51959986/how-to-solve-selenium-chromedriver-timed-out-receiving-message-from-renderer-exc
+//        driver = new ChromeDriver(options);
+        driver = new ChromeDriver(options);
         driver.get("http://accounts.google.com/");
         // a guarantee that the test was really executed
 //        driver.findElement(By.id("q")).isDisplayed();
